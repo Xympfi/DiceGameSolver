3b01fb6c04a1f10dd3915229f18741296822d10d
Xympfi <Xympfi@gmail.com>
2022-09-04 22:46:38 +0200

    added web server support

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse, HTMLResponse
+import random
 import json
 import dicelib
 
@@ -7,24 +10,27 @@ multiples = [1,1,1,10,10,10, 100, 100, 100,1000, 1000,1000,10000, 10000,10000]
 targets = range(1,120)
 dice_values =[1,2,3,4,5,6]
 
-target = random.choice(targets)
-
-a = random.choice(dice_values)
-b = random.choice(dice_values)
-c = random.choice(dice_values)
-
-dice = [a,b,c]
-
-dice_combinations, operation_combinations, multiple_combinations = dicelib.generate_combinations(dice, operations, multiples)
-results = dicelib.calculate_all_solutions(target, dice_combinations, operation_combinations, multiple_combinations)
-
-target_dump = json.dumps([target])
-dice_json = json.dumps(dice)
-results_json = json.dumps(result[:15])
-
 app = FastAPI()
 
-
+#app.mount("/", StaticFiles(directory="static",html = True), name="static")
 @app.get("/")
+async def homepage():
+    file = open("./static/index.html","r")
+    html_content =str(file.read())
+    file.close()
+    return HTMLResponse(content = html_content, status_code = 200)
+
+
+@app.get("/dicegame", response_class=JSONResponse)
 def read_root():
-    return dice_json
+    target = random.choice(targets)
+    a = random.choice(dice_values)
+    b = random.choice(dice_values)
+    c = random.choice(dice_values)
+    dice = [a,b,c]
+    dice_combinations, operation_combinations, multiple_combinations = dicelib.generate_combinations(dice, operations, multiples)
+    results = dicelib.calculate_all_solutions(target, dice_combinations, operation_combinations, multiple_combinations)
+    rslts =[]
+    for r in results:
+        rslts.append(str(r[9])+":"+str(r[8])+"="+"("+str(r[0]*r[1])+r[2]+str(r[3]*r[4])+")"+r[5]+str(r[6]*r[7]))
+    return {"Target": target, "dice": dice, "Results": rslts[0:15]} 
