cf22c27c7d6e645d1f2006d3ab465ed42836f07c
Xympfi <Xympfi@gmail.com>
2022-09-05 12:24:47 +0200

    added some more comments

--- server.py
+++ server.py
@@ -5,14 +5,13 @@ import random
 #import json
 import dicelib
 
-operations =["+","-","*","/"]
-multiples = [1,1,1,10,10,10, 100, 100, 100,1000, 1000,1000,10000, 10000,10000]
-targets = range(1,120)
-dice_values =[1,2,3,4,5,6]
+#uvicorn web server needs to be running in local environment
+
 
 app = FastAPI()
 
 #app.mount("/", StaticFiles(directory="static",html = True), name="static")
+# return the contents of intex.htlm file in folder static when the https://host/ is called in a browser
 @app.get("/")
 async def homepage():
     file = open("./static/index.html","r")
@@ -21,8 +20,13 @@ async def homepage():
     return HTMLResponse(content = html_content, status_code = 200)
 
 
+# return the target value, dice values and first 15 solutions in JSON format when the https://host/dicegame is called in a browser
 @app.get("/dicegame", response_class=JSONResponse)
 def read_root():
+    operations =["+","-","*","/"]
+    multiples = [1,1,1,10,10,10, 100, 100, 100,1000, 1000,1000,10000, 10000,10000]
+    targets = range(1,120)
+    dice_values =[1,2,3,4,5,6]
     target = random.choice(targets)
     a = random.choice(dice_values)
     b = random.choice(dice_values)
